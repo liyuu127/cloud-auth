e002d66d5ac79ba28a1874aa43624ee0b289d83b
liyu <liyu@weiyankeji.cn>
2023-06-27 11:52:04 +0800

    feat<server>:内存授权实现

---
+++ login/src/main/java/cn/liyu/OAuth2LoginApplication.java
@@ -0,0 +1,17 @@
+package cn.liyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * OAuth2 Log In application.
+ *
+ */
+@SpringBootApplication
+public class OAuth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2LoginApplication.class, args);
+	}
+
+}
