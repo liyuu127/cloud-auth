0d375cfb05de44e0e5eec2c6a7ae93bba66351ac
liyu <liyu@weiyankeji.cn>
2023-06-29 15:57:28 +0800

    oauth2授权示例

---
+++ federated-identity-authorizationserver/src/main/java/cn/liyu/FederatedIdentityAuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package cn.liyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FederatedIdentityAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederatedIdentityAuthorizationServerApplication.class, args);
+	}
+
+}
